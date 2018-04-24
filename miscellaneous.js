d129c4b3040969720be882ba1267bb82df79d507
zhangchengcheng <627395160@qq.com>
2018-04-24 13:57:56 +0800

    '杂项'

---
+++ miscellaneous.js
@@ -0,0 +1,7 @@
+[] == false  // true
+[] == ![]    // true
+
+数组: arr.push()  arr instanceof Array  Object.prototype.toString.call(arr) == '[Object, Array]'  Array.isArray();
+ 
+Array.from()   [...]  Array.prototype.slice.call();
+
